feb05295db3bcbb12e99121b4b0e5fe29fd1f8c4
TSRSE <26186238+TSRSE@users.noreply.github.com>
2023-10-23 23:25:09 +0300

    Тестовый рабочий вариант карточек Работает на странице ../pages/test_page Нужно опробовать fetching / получение списка с бекенда

--- front-end/scripts/cardsController.js
+++ front-end/scripts/cardsController.js
@@ -1,5 +1,11 @@
 let CardsArray = []
 
+
+function CheckForStage(array, state){
+  // console.log(array.find((item => item[`${state}`] == false)))
+  return array.find((item => item[`${state}`] == false)) === undefined ? true : false;
+}
+
 function updateInArray(array, id, updatedItem){
   let elementId = array.findIndex((item => item.id == id));
   array[elementId] = updatedItem;
@@ -7,26 +13,33 @@ function updateInArray(array, id, updatedItem){
 
 class Card{
   constructor(card){
-    this.id = card.id;
-    this.OS = card.OS;
-    this.status = card.Status;
-    // this.code = card.Code;
-    this.Browser = card.Browser;
-    this.ScreenPreview = card.LINKS[0].ScreenPreview;
-    this.ScreenFull = card.LINKS[0].ScreenFull;
-    this.DiffPreview = card.LINKS[0].DiffPreview;
-    this.DiffFull = card.LINKS[0].DiffFull;
+    if(card !== undefined) 
+    {
+      this.id = card.id;
+      this.OS = card.OS;
+      this.status = card.Status;
+      this.Browser = card.Browser;
+      this.ScreenPreview = card.LINKS[0].ScreenPreview;
+      this.ScreenFullLink = card.LINKS[0].ScreenFullLink;
+      this.DiffPreview = card.LINKS[0].DiffPreview;
+      this.DiffFullLink = card.LINKS[0].DiffFullLink;
 
-    this.hasDiffPreview = false;//card.LINKS[0].DiffPreview == '' ? false : true;
+      this.diffIsShowing = false;
+      this.readyToCreateDiff = this.ScreenPreview != '' && this.ScreenFullLink != '' && this.status == '200';
+      this.readyToShowDiff = this.DiffPreview != '' && this.DiffFullLink != '' && this.status == '200';
+    }
   }
 
   diffPreviewToggle(){
-    this.hasDiffPreview = !this.hasDiffPreview;
+    this.diffIsShowing = !this.diffIsShowing;
     updateInArray(CardsArray, this.id, this);
     display()
   }
 
   render(rootNode){
+    if(this.id === undefined) 
+    {return null;}
+
     let _card = document.createElement("div"); _card.className = "render-card";
 // Верхняя часть карточки
     let cardTop = document.createElement("div"); cardTop.className = "render-card--top";
@@ -42,7 +55,7 @@ class Card{
 
     //Left
     let leftSide = document.createElement("div"); leftSide.classList.add("render-card--state-display--left");
-    let previewImg = document.createElement("img"); previewImg.setAttribute("src", "../src/img/cards/placeholder.png");
+    let previewImg = document.createElement("img"); previewImg.setAttribute("src", this.DiffPreview == '' ? "../src/img/cards/placeholder.png" : this.DiffPreview);
     leftSide.appendChild(previewImg);
 
     // Отображение статуса
@@ -53,36 +66,25 @@ class Card{
 
     let messageIcon = null;
 
+    function createContextByStatus(className, text, messageIconPath){
+      status = document.createElement("div"); status.classList.add(className);
+      message = document.createElement("p"); message.textContent = text;
+      status.appendChild(message);
+      messageIcon = document.createElement("img"); messageIcon.setAttribute("src", messageIconPath);
+      status.appendChild(messageIcon);
+    }
+
     switch (this.status.toUpperCase()) {
       case '200':
-      status = document.createElement("div"); status.classList.add("card-state");
-
-      message = document.createElement("p"); message.textContent = "Будет выводиться скриншот";
-      status.appendChild(message);
-
-      messageIcon = document.createElement("img"); messageIcon.setAttribute("src", "../src/icons/green-check.svg");
-      status.appendChild(messageIcon);
+        createContextByStatus("card-state", "", this.ScreenPreview);
         break;
 
       case 'PENDING':
-        status = document.createElement("div"); status.classList.add("animation--card-state");
-
-        message = document.createElement("p"); message.textContent = "В обработке";
-        status.appendChild(message);
-
-        messageIcon = document.createElement("img"); messageIcon.setAttribute("src", "../src/icons/loading.svg");
-        status.appendChild(messageIcon);
-    break;
+        createContextByStatus("animation--card-state", "В обработке", "../src/icons/loading.svg");
+        break;
 
       default: //ERROR
-        status = document.createElement("div"); status.classList.add("card-state");
-
-        message = document.createElement("p"); message.textContent = "Ошибка";
-        status.appendChild(message);
-
-        messageIcon = document.createElement("img"); messageIcon.setAttribute("src", "../src/icons/red-cross.svg");
-        status.appendChild(messageIcon);
-        
+        createContextByStatus("card-state", "Ошибка", "../src/icons/red-cross.svg");        
         break;
     }
     
@@ -92,8 +94,8 @@ class Card{
 
     cardMiddle.appendChild(leftSide);
 
-    //Rigth
-    if (this.hasDiffPreview) {
+    //Правая часть
+    if (this.diffIsShowing) {
       
       let rightSide = document.createElement("div"); rightSide.classList.add("render-card--state-display--right"); 
       let dash = document.createElement("div"); dash.classList.add("vertical-dash");
@@ -111,39 +113,53 @@ class Card{
     _card.appendChild(cardMiddle)
 
 
-    // Конец карточки
     let cardBottom = document.createElement("div"); 
-    cardBottom.classList.add("render-card--bottom");
-    cardBottom.classList.add("flex");
-    cardBottom.classList.add("center");
+      cardBottom.classList.add("render-card--bottom");
+      cardBottom.classList.add("flex");
+      cardBottom.classList.add("center");
 
-    let radioAsExample = document.createElement("input"); 
-    radioAsExample.setAttribute("type", "radio"); 
-    radioAsExample.setAttribute("name", "card-radio");
-    radioAsExample.classList.add("hidden");
+    // TODO Улучшить читаемость кода
+    // Конец карточки
+    if(CheckForStage(CardsArray, 'readyToCreateDiff') && !CheckForStage(CardsArray, 'readyToShowDiff')){
+      
 
-    cardBottom.appendChild(radioAsExample);
+      // Radio для выбора образца 
+      let radioAsExample = document.createElement("input"); 
+      radioAsExample.setAttribute("type", "radio"); 
+      radioAsExample.setAttribute("name", "cardRadio");
 
-    let showDiffCheckbox = document.createElement("input"); 
-    showDiffCheckbox.setAttribute("type", "checkbox"); 
-    showDiffCheckbox.setAttribute("name", "diffCheckBox");
-    showDiffCheckbox.checked = this.hasDiffPreview;
-    showDiffCheckbox.onchange = () => this.diffPreviewToggle();
-    // showDiffCheckbox.classList.add("hidden");
+      let labelForRadio = document.createElement("label");
+      labelForRadio.setAttribute("for","cardRadio");
 
-    cardBottom.appendChild(showDiffCheckbox);
+      let innerlabelForRadio = document.createElement("p");
+      innerlabelForRadio.textContent = "Образец для сравнения"
 
-    let labelForCheckbox = document.createElement("label");
-    labelForCheckbox.setAttribute("for","diffCheckBox");
+      labelForRadio.appendChild(innerlabelForRadio)
+      
 
-    let innerLabel = document.createElement("p");
-    innerLabel.textContent = this.hasDiffPreview ?  "Свернуть сравнение <" : "Развернуть сравнение >"
+      cardBottom.appendChild(radioAsExample);
+      cardBottom.appendChild(labelForRadio);
+    }
 
-    labelForCheckbox.appendChild(innerLabel)
-    // labelForCheckbox.setAttribute("text","cheese")
-    cardBottom.appendChild(labelForCheckbox);
-    
-    
+    if (CheckForStage(CardsArray, 'readyToCreateDiff') && CheckForStage(CardsArray, 'readyToShowDiff')) {
+      // Checkbox для демонстрации сравнения
+      let showDiffCheckbox = document.createElement("input"); 
+      showDiffCheckbox.setAttribute("type", "checkbox"); 
+      showDiffCheckbox.setAttribute("name", "diffCheckBox");
+      showDiffCheckbox.checked = this.diffIsShowing;
+      showDiffCheckbox.onchange = () => this.diffPreviewToggle();
+
+      cardBottom.appendChild(showDiffCheckbox);
+
+      let labelForCheckbox = document.createElement("label");
+      labelForCheckbox.setAttribute("for","diffCheckBox");
+
+      let innerLabel = document.createElement("p");
+      innerLabel.textContent = this.diffIsShowing ?  "Свернуть сравнение <" : "Развернуть сравнение >"
+
+      labelForCheckbox.appendChild(innerLabel)
+      cardBottom.appendChild(labelForCheckbox);
+    }
     
     _card.appendChild(cardBottom);
 
@@ -151,10 +167,11 @@ class Card{
   }
 }
 
-let obj = { "id": 0, "OS": "WIN","Browser": "edge", "Status" : "Обрабатывается", "LINKS":[{"ScreenPreview": "", "ScreenFull": "", "DiffPreview": "", "DiffFull": ""}] }
+let obj = { "id": 0, "OS": "WIN","Browser": "edge", "Status" : "Обрабатывается", "LINKS":[{"ScreenPreview": "w", "ScreenFullLink": "w", "DiffPreview": "", "DiffFullLink": ""}] }
 CardsArray.push(new Card(obj))
-CardsArray.push(new Card({ "id": 1, "OS": "MAC", "Status" : "Pending","Browser": "Safari", "LINKS":[{"ScreenPreview": "", "ScreenFull": "", "DiffPreview": " w", "DiffFull": ""}]} ))
-CardsArray.push(new Card({ "id": 2, "OS": "LIN", "Status" : "200", "Browser": "FIREfox", "LINKS":[{"ScreenPreview": "", "ScreenFull": "", "DiffPreview": "", "DiffFull": ""}]} ))
+CardsArray.push(new Card({ "id": 1, "OS": "MAC", "Status" : "Pending","Browser": "Safari", "LINKS":[{"ScreenPreview": "w", "ScreenFullLink": "w", "DiffPreview": "", "DiffFullLink": ""}]} ))
+CardsArray.push(new Card({ "id": 2, "OS": "LIN", "Status" : "200", "Browser": "FIREfox", "LINKS":[{"ScreenPreview": "w", "ScreenFullLink": "w", "DiffPreview": "", "DiffFullLink": ""}]} ))
+CardsArray.push(new Card(undefined))
 
 function display(){
   let parent = document.getElementById("__UUID");
@@ -165,7 +182,3 @@ function display(){
 }
 
 display();
-
-function updateView(){
-
-}
