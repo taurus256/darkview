c0ef830d9515d72ac8cca30112aa6471799f27c6
Евгений Остапенко <ostapenko.evgenii@otr.ru>
2024-03-25 18:28:31 +0300

    Исправлена отрисовка карточек на странице result (NATA) -2

--- front-end/scripts/cardsController.js
+++ front-end/scripts/cardsController.js
@@ -1,44 +1,268 @@
-let STAGES = ['Default', 'ReadyToCreateDiff', 'ReadyToShowDiff']
+let STAGES = { DEFAULT : 0, READY_TO_CREATE_DIFF : 1, READY_TO_SHOW_DIFF : 1 };
 
-let TaskStage = STAGES[0]; // DEFAULT
-let CardsMap = new Map();
-let TaskUUID = '';
-let CardsToRedraw = [];
+let HTML_CLASS = {
+  CARD_CONTAINER : "render-card",
+  CARD_TOP : "render-card--top",
+  CARD_MIDDLE : "render-card--state-display",
+  CARD_STATE_DISPLAY : "render-card--state-display--left"
+};
 
-class Card{
-  constructor(card){
-    if(card === undefined) 
-    {return;}
-    
-    //this.UUID = card.UUID;
-    this.id = card.id;
-    this.OS = card.OS;
-    this.status = card.Status;
-    this.Browser = card.Browser;
-    this.ScreenPreview = card.LINKS[0].ScreenPreview;
-    this.ScreenFullLink = card.LINKS[0].ScreenFullLink;
-    this.DiffPreview = card.LINKS[0].DiffPreview;
-    this.DiffFullLink = card.LINKS[0].DiffFullLink;
+let HTML_CONSTANTS {
+  SCN_PREVIEW_URL_DEFAULT : "../src/img/cards/placeholder.png",
+  SCN_PREVIEW_URL_PRE : '/job'
+};
 
+
+// Генератор случайных строк (для уникальных коротких id в html)
+function generator_rnd_uuid () {
+  let res_len = 4;
+  let res = '';
+  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+  let len = chars.length;
+  for ( let i=0; i < res_len; i++) {
+    let pos = Math.floor ( Math.random() * len );
+    res += chars.charAt(pos);
+  }
+  return res;
+}
+
+
+class Task {
+  constructor ( task_uuid ) {
+    this.uuid = task_uuid;
+    this.jobsMap = new Map();
+    this.toRedrawArray = [];
+    this.stage = STAGES.DEFAULT;
+    this.uuidHTML = generator_rnd_uuid();
+  }
+}
+
+class Job {
+
+  constructor ( json_data ) {
+
+    if( json_data === undefined ) { return; }
+
+    this.id = json_data.id;
+    this.os = json_data.OS.toUpperCase();
+    this.status = json_data.Status;
+    this.browser = json_data.Browser.toUpperCase();
+
+    this.idHTML = generator_rnd_uuid();
+    this.osIconPathHTML = `../src/icons/systems/${this.os}.png`;
+
+    // Ссылка на превью скриншота (url)
+    this.scnPreviewURL = json_data.LINKS[0].ScreenPreview;
+    // Ссылка на полный скриншот (url)
+    this.scnURL = json_data.LINKS[0].ScreenFullLink;
+    // Ссылка на превью карты различий (url)
+    this.diffPreviewURL = json_data.LINKS[0].DiffPreview;
+    // Ссылка на полную карту различий (url)
+    this.diffURL = json_data.LINKS[0].DiffFullLink;
+
+    // статус показа превью карты различий в данный помент (true / false)
     this.diffIsShowing = false;
-    this.readyToCreateDiff = this.status === 'SUCCESS';
-    this.readyToShowDiff = this.DiffPreview != undefined && this.status === 'PREVIEW_IS_READY';
-    
-    this.stage = card.Stage;
   }
 
-  diffPreviewToggle(){
-    this.diffIsShowing = !this.diffIsShowing;
-    updateInArray (this.id, this);
-    display();
+  // отрисовка карточки, которой еще не было
+  render_card_create_html ( cardHTML ) {
+
+    cardHTML = document.createElement("div");
+    cardHTML.className = HTML_CLASS.CARD_CONTAINER;
+    cardHTML.setAttribute("id", this.idHTML );
+
+    // Верхняя часть карточки
+
+    let cardTopHTML = document.createElement("div");
+    cardTopHTML.className = HTML_CLASS_CARD_TOP;
+
+    let osIconHTML = document.createElement("img");
+    osIconHTML.setAttribute ("src", this.osIconPathHTML );
+
+    let browserNameHTML = document.createElement ("h5");
+    browserNameHTML.textContent = this.browser;
+
+    cardTopHTML.appendChild(osIconHTML);
+    cardTopHTML.appendChild(browserNameHTML);
+    cardHTML.appendChild(cardTopHTML);
+
+    // Средняя часть карточки
+
+    let cardMiddleHTML = document.createElement("div");
+    cardMiddleHTML.classList.add( HTML_CLASS.CARD_MIDDLE );
+   
+    // Средняя часть карточки - левая сторона (превью скриншота, статус обработки)
+
+    let leftSideHTML = document.createElement("div");
+    leftSideHTML.classList.add( HTML_CLASS.CARD_STATE_DISPLAY );
+
+    let scnPreviewHTML = document.createElement("img");
+    let scnPreviewPath = HTML_CONSTANTS.SCN_PREVIEW_URL_DEFAULT;
+    if ( this.scnPreviewURL !== undefined && this.scnPreviewURL != '' ) {
+      scnPreviewPath = '/job' + this.scnPreviewURL;
+    }
+    previewImg.setAttribute ("src", scnPreviewPath );
+
+    previewImg.addEventListener ( 'click', () => {
+      window.open ( '/job' + this.scnURL, '_blank').focus();
+    });
+
+   leftSide.appendChild(previewImg);
+   
+   // Левая часть: Отображение статуса
+   let htmlStatus = null;
+   let htmlMessage = null;
+   let htmlMessageIcon = null;
+   let htmlRequest = null;
+   let curCardId = this.id;
+
+   function createContextByStatus ( className, text, messageIconPath, requestText ) {
+     htmlStatus = document.createElement("div");
+     htmlStatus.classList.add(className);
+     htmlMessage = document.createElement("p");
+     htmlMessage.textContent = text;
+     htmlStatus.appendChild ( htmlMessage );
+     htmlMessageIcon = document.createElement("img");
+     htmlMessageIcon.setAttribute ("src", messageIconPath);
+     htmlStatus.appendChild(htmlMessageIcon);
+     htmlRequest = document.createElement("a");
+     htmlRequest.textContent = requestText;
+     htmlRequest.classList.add('none');
+     htmlRequest.addEventListener('click', () => { retry ( curCardId ); display()});
+     htmlStatus.appendChild( htmlRequest );
+   }
+   switch (this.status.toUpperCase()) {
+     case 'CREATED':
+       createContextByStatus("card-state", "Задание в очереди", "", "");
+       break;
+     case 'SUCCESS':
+       createContextByStatus("card-state", "", "");
+       break;
+     
+     case 'PREVIEW_IS_READY':
+       createContextByStatus("card-state", "", "");
+       break;
+
+     case 'IN_PROGRESS':
+       createContextByStatus("animation--card-state", "В обработке", "../src/icons/informative/loading.svg");
+       break;
+
+     default: //ERROR
+       createContextByStatus("card-state", "Не удалось отрисовать", "../src/icons/informative/sad-failed.svg", "Попробовать еще раз");
+       break;
+   }
+   leftSide.appendChild ( htmlStatus );
+   // Левая часть: Конец отображения статуса
+   cardMiddle.appendChild(leftSide);
+   // Левая часть: конец
+
+   // Правая часть
+   let rightSide = document.createElement("div");
+   rightSide.classList.add("render-card--state-display--right"); 
+   
+   if (this.status.toUpperCase() === 'PREVIEW_IS_READY') {
+     console.log("render id=" + this.id + ", PREVIEW_IS_READY");
+     let dash = document.createElement("div");
+     dash.classList.add("vertical-dash");
+     rightSide.appendChild(dash);
+
+     let diffPicture = document.createElement("img");
+     diffPicture.setAttribute("src", '/job' + this.DiffPreview);
+     diffPicture.addEventListener('click', () => {window.open(`${previewUrl}${this.DiffFullLink}`, '_blank').focus();})
+     rightSide.appendChild(diffPicture);
+
+     cardMiddle.appendChild(rightSide);
+   }
+
+   // НИЖНЯЯ ЧАСТЬ
+   let cardBottom = null;
+   if ( is_new_flag === true ) {
+       cardBottom = document.createElement("div"); 
+       cardBottom.classList.add("render-card--bottom");
+       cardBottom.classList.add("flex");
+       cardBottom.classList.add("center");
+   } else {
+     cardBottom = _card.getElementsByClassName("render-card--bottom").item(0);
+     cardBottom.innerHTML = '';
+   }
+
+   // Если сравнение скриншотов ещё не происходило
+   if(this.stage == STAGES[1]) {
+     console.log("STAGE 1 for id = " + this.id )
+     // Галочка для выбора данной карточки за образец 
+     let radioAsExample = document.createElement("input"); 
+     radioAsExample.setAttribute("type", "radio"); 
+     radioAsExample.setAttribute("id", "cardRadio" + this.id );
+     radioAsExample.setAttribute("name", "cardRadio" );
+     radioAsExample.onchange = () => {
+         if (radioAsExample.checked) {
+           window.popup.showButton (
+             'Нажмите кнопку "Старт" для запуска сравнения',
+             'Старт',
+             () => sendRequestToDiff( this.id)
+             );
+         }
+     };
+
+     let labelForRadio = document.createElement("label");
+     labelForRadio.setAttribute("for","cardRadio" + this.id );
+
+     let innerlabelForRadio = document.createElement("p");
+     innerlabelForRadio.textContent = "Образец для сравнения";
+
+     labelForRadio.appendChild(innerlabelForRadio);
+     
+     cardBottom.appendChild(radioAsExample);
+     cardBottom.appendChild(labelForRadio);
+   }
+
+   if (this.DiffPreview != '' && this.DiffPreview != undefined) {
+     // Checkbox для демонстрации сравнения
+     let showDiffCheckbox = document.createElement("input"); 
+     showDiffCheckbox.setAttribute("type", "checkbox" );
+     showDiffCheckbox.setAttribute("id", "diffCheckBox" + this.id );
+     showDiffCheckbox.setAttribute("name", "diffCheckBox" );
+     showDiffCheckbox.checked = this.diffIsShowing;
+     showDiffCheckbox.onchange = () => this.diffPreviewToggle();
+
+     cardBottom.appendChild(showDiffCheckbox);
+
+     let labelForCheckbox = document.createElement("label");
+     labelForCheckbox.setAttribute("for","diffCheckBox" + this.id);
+
+     let innerLabel = document.createElement("p");
+     innerLabel.textContent = this.diffIsShowing ?  "Свернуть сравнение <" : "Развернуть сравнение >"
+     this.diffIsShowing ? _card.className = 'render-card--big' : 'render-card';
+     this.diffIsShowing ? rightSide.classList.remove('hidden') : rightSide.classList.add('hidden')
+
+     labelForCheckbox.appendChild(innerLabel)
+     cardBottom.appendChild(labelForCheckbox);
+   }
+   
+   if ( is_new_flag === true ) {
+     _card.appendChild(cardMiddle);
+     _card.appendChild(cardBottom);
+     rootNode.appendChild(_card);
+   }
+ }
   }
 
-  render (rootNode) {
-    if(this.id === undefined) 
-    {return null;}
+  // перерисовка карточки, которая уже была
+  render_card_update_html ( cardHTML ) {
 
-    let _card = document.getElementById(this.id);
-    let is_new_flag = false;
+  }
+
+  render_card_html ( rootNode ) {
+
+    if ( this.id === undefined ) { return null; }
+
+    let cardHTML = document.getElementById ( this.id);
+    if ( cardHTML === null ) {
+      this.render_card_create_html ( cardHTML );
+    } else {
+      this.render_card_update_html ( cardHTML );
+    }
+/*
     if ( _card === null ) {
        _card = document.createElement("div");
        _card.className = "render-card";
@@ -216,6 +440,7 @@ class Card{
       rootNode.appendChild(_card);
     }
   }
+  */
 }
 
 function CheckForStage () {
